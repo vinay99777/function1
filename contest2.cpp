49082e77ca9ae34d3072515b9aa79b867b2e389c
vinay99777 <vinaybagul18@gmail.com>
2021-02-13 22:52:18 +0530

    test

---
+++ contest2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i,j,n,a;
+     cin>>n;
+    int ar[n];
+   
+    for(i=1;i<=n;i++)
+    {
+        int sum=0;
+        cin>>a;
+        for(j=1;j<=a;j++)
+        {
+            cin>>ar[j];
+        }
+        for(j=1;j<a;j++)
+        {
+            sum=sum+abs(ar[j]-ar[j+1]);
+            
+        }
+        cout<<sum+abs(ar[a]-ar[1])<<endl;
+        cout<<abs(ar[a]-ar[1]);
+        
+    }
+}
+
